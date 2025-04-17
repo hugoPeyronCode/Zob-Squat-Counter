103661d3be65f95b0857452d62617a9ca10178b7
HugoPVoodoo <hugo.peyron@voodoo.io>
2025-04-17 20:28:01 +0200

    Initial Commit

---
+++ Zob Squat Counter/Zob_Squat_CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Zob_Squat_CounterApp.swift
+//  Zob Squat Counter
+//
+//  Created by Hugo Peyron on 17/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct Zob_Squat_CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
