c8fc3206b974e745c225b22277c72a2aaf6df491
HugoPVoodoo <hugo.peyron@voodoo.io>
2025-04-18 15:16:15 +0200

    Widget configuation added. Now needs to be redifined

---
+++ SquatWidgetExtension/SquatWidgetExtensionBundle.swift
@@ -0,0 +1,18 @@
+//
+//  SquatWidgetExtensionBundle.swift
+//  SquatWidgetExtension
+//
+//  Created by Hugo Peyron on 18/04/2025.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct SquatWidgetExtensionBundle: WidgetBundle {
+    var body: some Widget {
+        SquatWidgetExtension()
+        SquatWidgetExtensionControl()
+        SquatWidgetExtensionLiveActivity()
+    }
+}
