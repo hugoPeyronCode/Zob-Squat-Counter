4a7e7f48bbb074f099971b59cf3e9081a24afc4e
HugoPVoodoo <hugo.peyron@voodoo.io>
2025-04-20 16:40:00 +0200

    widget UI updated

---
+++ SquatWidgetExtension/Widgets/StatsWidget.swift
@@ -0,0 +1,8 @@
+//
+//  StatsWidget.swift
+//  SquatWidgetExtensionExtension
+//
+//  Created by Hugo Peyron on 20/04/2025.
+//
+
+import Foundation
---
+++ SquatWidgetExtension/Widgets/StreakWidget.swift
@@ -0,0 +1,18 @@
+//
+//  StreakWidget.swift
+//  SquatWidgetExtensionExtension
+//
+//  Created by Hugo Peyron on 20/04/2025.
+//
+
+import SwiftUI
+
+struct StreakWidget: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StreakWidget()
+}
